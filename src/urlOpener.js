31404dd702c8ee6e3fe00733347995d262202601
ZipFile <lin.aaa.lin@gmail.com>
2022-01-30 14:12:03 +0000

    Add url opener abstraction

---
+++ src/urlOpener.js
@@ -0,0 +1,65 @@
+export class NotImplementedError extends Error {}
+
+export class URLOpener {
+    // eslint-disable-next-line no-unused-vars
+    async current(url) {
+        throw new NotImplementedError();
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    async new(url, background) {
+        throw new NotImplementedError();
+    }
+
+    open(url, openIn) {
+        switch (openIn) {
+        case "new": return this.new(url, false);
+        case "background": return this.new(url, true);
+        case "current": return this.current(url);
+        default: throw new Error(`invalid openIn: ${openIn}`);
+        }
+    }
+}
+
+export class GenericURLOpener extends URLOpener {
+    constructor(browser, tab) {
+        super();
+
+        this.browser = browser;
+        this.tab = tab;
+    }
+
+    current(url) {
+        return this.browser.tabs.update(this.tab.id, {url});
+    }
+
+    get newTabParams() {
+        throw new NotImplementedError();
+    }
+
+    new(url, background) {
+        return this.browser.tabs.create({
+            active: !background,
+            url: url,
+            ...this.newTabParams,
+        });
+    }
+}
+
+export class BrowserURLOpener extends GenericURLOpener {
+    get newTabParams() {
+        return {openerTabId: this.tab.id};
+    }
+}
+
+export class AndroidURLOpener extends GenericURLOpener {
+    get newTabParams() {
+        return {};
+    }
+}
+
+export class ChromeURLOpener extends GenericURLOpener {
+    get newTabParams() {
+        return {index: this.tab.index + 1, openerTabId: this.tab.id};
+    }
+}
---
+++ test/test_urlOpener.js
@@ -0,0 +1,155 @@
+import should from "should/as-function.js";
+
+import {
+    NotImplementedError,
+    URLOpener,
+    GenericURLOpener,
+    BrowserURLOpener,
+    AndroidURLOpener,
+    ChromeURLOpener,
+} from "upload-to-danbooru/urlOpener.js";
+
+describe("URLOpener", function() {
+    const url = "https://example.com";
+    const urlOpener = new URLOpener();
+
+    it("current()", function() {
+        should(urlOpener.current(url)).rejectedWith(NotImplementedError);
+    });
+
+    it("new()", function() {
+        should(urlOpener.new(url, true)).rejectedWith(NotImplementedError);
+    });
+
+    class TestURLOpener extends URLOpener {
+        async current(url) {
+            return {url}
+        }
+
+        async new(url, background) {
+            return {url, background}
+        }
+    }
+
+    const testUrlOpener = new TestURLOpener();
+
+    it("open() new", async function() {
+        const result = await testUrlOpener.open(url, "new");
+
+        should(result).deepEqual({url, background: false});
+    });
+
+    it("open() background", async function() {
+        const result = await testUrlOpener.open(url, "background");
+
+        should(result).deepEqual({url, background: true});
+    });
+
+    it("open() current", async function() {
+        const result = await testUrlOpener.open(url, "current");
+
+        should(result).deepEqual({url});
+    });
+
+    it("open() unknown", function() {
+        should(() => testUrlOpener.open(url, "test")).throw("invalid openIn: test");
+    });
+});
+
+describe("GenericURLOpener", function() {
+    const url = "https://example.com";
+    const tab = {id: 123};
+
+    class TestURLOpener extends GenericURLOpener {
+        get newTabParams() {
+            return {test: `x${this.tab.id}`};
+        }
+    }
+
+    it("current()", async function() {
+        const update = [];
+        const browser = {
+            tabs: {
+                async update(tabId, params) {
+                    update.push([tabId, params]);
+                }
+            },
+        };
+        const urlOpener = new TestURLOpener(browser, tab);
+
+        await urlOpener.current(url);
+
+        should(update).deepEqual([[tab.id, {url}]]);
+    });
+
+    it("new() active", async function() {
+        const create = [];
+        const browser = {
+            tabs: {
+                async create(params) {
+                    create.push(params);
+                }
+            },
+        };
+        const urlOpener = new TestURLOpener(browser, tab);
+
+        await urlOpener.new(url, false);
+
+        should(create).deepEqual([{url, active: true, test: "x123"}]);
+    });
+
+    it("new() background", async function() {
+        const create = [];
+        const browser = {
+            tabs: {
+                async create(params) {
+                    create.push(params);
+                }
+            },
+        };
+        const urlOpener = new TestURLOpener(browser, tab);
+
+        await urlOpener.new(url, false);
+
+        should(create).deepEqual([{url, active: true, test: "x123"}]);
+    });
+});
+
+describe("BrowserURLOpener", function() {
+    const url = "https://example.com";
+    const urlOpener = new BrowserURLOpener(undefined, {id: 123});
+
+    it("subclass of GenericURLOpener", function() {
+        should(urlOpener).instanceof(GenericURLOpener);
+    });
+
+    it("get newTabParams()", function() {
+        should(urlOpener.newTabParams).deepEqual({openerTabId: 123});
+    });
+});
+
+describe("AndroidURLOpener", function() {
+    const url = "https://example.com";
+    const urlOpener = new AndroidURLOpener();
+
+    it("subclass of GenericURLOpener", function() {
+        should(urlOpener).instanceof(GenericURLOpener);
+    });
+
+    it("get newTabParams()", function() {
+        should(urlOpener.newTabParams).deepEqual({});
+    });
+});
+
+describe("ChromeURLOpener", function() {
+    const url = "https://example.com";
+    const urlOpener = new ChromeURLOpener(undefined, {id: 123, index: 5});
+
+    it("subclass of GenericURLOpener", function() {
+        should(urlOpener).instanceof(GenericURLOpener);
+    });
+
+    it("get newTabParams()", function() {
+        should(urlOpener.newTabParams).deepEqual({openerTabId: 123, index: 6});
+    });
+});
