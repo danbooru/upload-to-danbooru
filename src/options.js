b5ba919eb90b48879fa007f1c3067dcb3daf6c1f
ZipFile <lin.aaa.lin@gmail.com>
2022-07-16 11:30:47 +0000

    Reinit context menus after saving the setting

--- src/options.js
+++ src/options.js
@@ -1,16 +1,29 @@
+import {
+    BrowserContextMenuManager,
+    ContextMenuSetupperImpl,
+} from "./contextMenuManager.js";
 import {BrowserStorageSettings, FormManager} from "./settings.js";
 import { DanbooruURL, getAPI } from "./utils.js";
 
 const [api, isChrome] = getAPI(globalThis);
 const form = document.forms.settings;
 const settings = new BrowserStorageSettings(api);
-const defaults = {url: "", pageActionOpenIn: "current", contextMenuOpenIn: "background"};
+const cm = new BrowserContextMenuManager(api);
+const cms = new ContextMenuSetupperImpl(cm, settings);
+const defaults = {
+    url: "",
+    pageActionOpenIn: "current",
+    contextMenuOpenIn: "background",
+    contextMenuEnabled: "yes",
+};
 const formManager = new FormManager(form, settings, defaults);
 
 async function saveOptions(e) {
     e.preventDefault();
 
-    await formManager.save("url", "pageActionOpenIn", "contextMenuOpenIn");
+    await formManager.save("url", "pageActionOpenIn", "contextMenuOpenIn", "contextMenuEnabled");
+
+    cms.setup();
 
     if (isChrome) {
         window.close();
@@ -20,7 +33,7 @@ async function saveOptions(e) {
 }
 
 async function restoreOptions() {
-    await formManager.load("url", "pageActionOpenIn", "contextMenuOpenIn");
+    await formManager.load("url", "pageActionOpenIn", "contextMenuOpenIn", "contextMenuEnabled");
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
